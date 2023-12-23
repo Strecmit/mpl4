136766cb346403ce209e352ab0802d2218db5694
Victor <strecmit@gmail.com>
2023-12-23 22:46:34 +0200

    task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,23 +1,32 @@
 ﻿int exit = 0, sum = 0, num = 0;
-while(exit == 0){
+string input = "s";
+while(exit == 0)
+{
     Console.Write("Ввод: ");
-    string input = Console.ReadLine();
-    if(input == "q") {
+    input = Console.ReadLine();
+    num = 0; sum = 0;
+    if((input == "q") || (input == "Q"))
+    {
         exit = 1;
     }      
     
-    else if(int.TryParse(input, out num)) {
-        while(num > 0){
-            sum = num % 10;
-            num = num / 10;
+    else if(int.TryParse(input, out num)) 
+    {
+        while(num > 0)
+        {
+            sum = num % 10 + sum;
+            num = (int) num / 10;          
         }
+        Console.WriteLine($"сумма цифр = {sum}");
+        if ((sum % 2) == 0)
+        {
+            Console.WriteLine($"Сумма цифр числа '{input}' чётная! Выход!"); exit = 2;
      
-    }
-    if ((sum % 2) == 0) {
-        Console.WriteLine("Сумма цифр числа чётная! Выход!"); exit = 1;
+        }
+    
     }
 
     else Console.WriteLine("Неверный ввод!");
 }
 
-Console.WriteLine("Введён символ 'q'! Выход!");
+if (exit == 1) Console.WriteLine($"Введён символ '{input}'! Выход!");
